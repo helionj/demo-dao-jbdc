8fbb34f0c43e8aea66d23e2919577ed52d81886e
helionporto <helionporto@hotmail.com>
2021-03-24 09:58:26 -0400

    findById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,6 +3,8 @@ package application;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -10,11 +12,9 @@ public class Program {
 
 	public static void main(String[] args) throws ParseException {
 		
-		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
-		Department department = new Department(1, "Eletronics");
-		
-		Seller seller = new Seller(1, "Paul Pereira", "pp@email.com", sdf.parse("25/06/1985"), 2500.00, department);
+		Seller seller = sellerDao.findById(3);
 		
 		System.out.println(seller);
 
--- src/model/entities/Department.java
+++ src/model/entities/Department.java
@@ -18,6 +18,10 @@ public class Department implements Serializable{
 	public Integer getId() {
 		return id;
 	}
+	
+	public void setId(Integer id) {
+		this.id = id;
+	}
 
 
 	public String getName() {
